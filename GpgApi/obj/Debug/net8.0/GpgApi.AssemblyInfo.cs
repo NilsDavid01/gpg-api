b47cd3b05e8fb8f541a7276a7b2f4852b2527568
NilsDavid01 <mail@davida.pro>
2026-02-04 14:55:09 +0100

    XunitTest

--- GpgApi/obj/Debug/net8.0/GpgApi.AssemblyInfo.cs
+++ GpgApi/obj/Debug/net8.0/GpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684fe83eb8c0b56ae412f82ba2278fe83f91d951")]
 [assembly: System.Reflection.AssemblyProductAttribute("GpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
